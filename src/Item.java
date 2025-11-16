285a7e75257516c38bc4ff17a5633610f9109208
fedippo <federicoippino@gmail.com>
2025-11-16 18:25:05 +0100

    implementazione e definizione classe Item

---
+++ src/Item.java
@@ -0,0 +1,23 @@
+abstract class Item {
+    private Attribute attribute;
+    private Object value;
+
+    public Item(Attribute attribute, Object value) {
+        this.attribute = attribute;
+        this.value = value;
+    }
+
+    public Attribute getAttribute() {
+        return attribute;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public String toString() {
+        return value.toString();
+    }
+
+    abstract double distance(Object a);
+}
