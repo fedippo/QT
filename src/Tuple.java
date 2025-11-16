8219e973951c321049ea2bfcd6e1dd7e9358988a
fedippo <federicoippino@gmail.com>
2025-11-16 18:38:05 +0100

    implementazione e definizione della classe Tuple

---
+++ src/Tuple.java
@@ -0,0 +1,38 @@
+public class Tuple {
+    private Item [] tuple;
+
+    public Tuple(int size) {
+        tuple = new Item[size];
+    }
+
+    public int getLength() {
+        return tuple.length;
+    }
+
+    public Item get(int i) {
+        return tuple[i];
+    }
+
+    void add(Item c, int i){
+        tuple[i] = c;
+    }
+
+    public double getDistance(Tuple obj) {
+        double distance=0.0;
+        for(int i=0;i<getLength();i++) {
+            distance += this.tuple[i].distance(obj.tuple[i]);
+        }
+        return distance;
+    }
+
+    public double avgDistance(Data data, int clusteredData[]){
+        double p=0.0,sumD=0.0;
+        for(int i=0;i<clusteredData.length;i++){
+            double d= getDistance(data.getItemSet(clusteredData[i]));
+            sumD+=d;
+        }
+        p=sumD/clusteredData.length;
+        return p;
+    }
+
+}
