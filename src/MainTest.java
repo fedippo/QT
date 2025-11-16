b4c109a9f6b6c8d07519d248294694a2533691b1
fedippo <federicoippino@gmail.com>
2025-11-16 22:38:47 +0100

    modifica del MainTest per ricevere in input il radius

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,5 +1,6 @@
 import data.Data;
 import mining.QTMiner;
+import static keyboardinput.Keyboard.*;
 
 public class MainTest {
 
@@ -8,16 +9,27 @@ public class MainTest {
 	 */
 	public static void main(String[] args) {
 		
-		
 		Data data =new Data();
 		System.out.println(data);
-		double radius =2.0;
-		QTMiner qt=new QTMiner(radius);
-		int numIter=qt.compute(data);
-		System.out.println("Number of clusters:"+numIter);
-		System.out.println(qt.getC().toString(data));
-		
-		
+
+		char choice = 'y';
+
+		do {
+			System.out.println("Insert radius (>0):");
+			double radius = readDouble();
+			QTMiner qt = new QTMiner(radius);
+			int numIter = qt.compute(data);
+			if(numIter == 1) {
+				System.out.println("14 tuples in one cluster!");
+				continue;
+			}
+			System.out.println("Number of clusters:" + numIter);
+			System.out.println(qt.getC().toString(data));
+			do {
+				System.out.println("New execution? (y/n)");
+				choice = readChar();
+			}while(choice != 'y' && choice != 'n');
+		}while(choice=='y');
 	}
 
 }
